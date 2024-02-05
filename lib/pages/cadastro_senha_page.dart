98967a11d783017fc53d59b77bd70e02e17ca7c3
Emerson Melo <emerson_sardinha@hotmail.com>
2024-02-05 19:01:26 -0300

    Criando tela de cadastro senha

--- lib/pages/cadastro_senha_page.dart
+++ lib/pages/cadastro_senha_page.dart
@@ -5,24 +5,23 @@ import 'package:lottie/lottie.dart';
 import '../helper/propriedades_globais.dart';
 import '../services/navigation_service.dart';
 import '../widgets/widgets_globais.dart';
-import 'cadastro_senha_page.dart';
 
-class CadastroEmailPage extends StatefulWidget {
+class CadastroSenhaPage extends StatefulWidget {
 
-  const CadastroEmailPage({Key? key}) : super(key: key);
+  const CadastroSenhaPage({Key? key}) : super(key: key);
 
   @override
-  State<CadastroEmailPage> createState() => _CadastroEmailPageState();
+  State<CadastroSenhaPage> createState() => _CadastroSenhaPageState();
 }
 
-class _CadastroEmailPageState extends State<CadastroEmailPage> {
+class _CadastroSenhaPageState extends State<CadastroSenhaPage> {
 
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: criarAppBar('Cadastrar e-mail'),
+      appBar: criarAppBar('Cadastrar senha'),
       backgroundColor: navbarColor,
       body: ScrollViewCustomizado(context),
     );
@@ -52,20 +51,20 @@ class _CadastroEmailPageState extends State<CadastroEmailPage> {
           children: [
             const SizedBox(height: 40),
             const Text(
-              'Insira um e-mail válido.',
+              'Insira uma senha.',
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
             ),
             const SizedBox(height: 20),
             const Padding(
               padding: EdgeInsets.only(left: 5.0, right: 5.0),
               child: Text(
-                'Olá, iremos te auxiliar a criar seu cadastro. Para prosseguir, é necessário inserir um e-mail válido. Fique tranquilo, suas informações estão protegidas.',
+                'Para finalizar seu cadastro, insira uma senha. Lembre-se, uma senha forte é composta por letras maiúsculas, minúsculas e números!',
                 style: TextStyle(color: Colors.white60),
                 textAlign: TextAlign.center,
               ),
             ),
             const SizedBox(height: 10),
-            Lottie.asset('lib/assets/register.json', height: 240),
+            Lottie.asset('lib/assets/security.json', height: 240),
             const SizedBox(height: 10),
             const TextField(
               keyboardType: TextInputType.emailAddress,
@@ -80,7 +79,7 @@ class _CadastroEmailPageState extends State<CadastroEmailPage> {
                   borderSide: BorderSide(color: Colors.deepPurple),
                   borderRadius: BorderRadius.all(Radius.circular(15)),
                 ),
-                hintText: 'Digite um e-mail válido',
+                hintText: 'Digite uma senha forte',
                 helperText: null,
                 helperStyle: TextStyle(
                   color: Colors.white,
@@ -93,7 +92,7 @@ class _CadastroEmailPageState extends State<CadastroEmailPage> {
             Expanded(
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: botaoCustom(context, 'PRÓXIMO', () => navigateToPage(context, const CadastroSenhaPage()),
+                child: botaoCustom(context, 'FINALIZAR', () => navigateToPage(context, const LoginPage()),
                     Colors.purple.shade900, Colors.white, true),
               ),
             ),
