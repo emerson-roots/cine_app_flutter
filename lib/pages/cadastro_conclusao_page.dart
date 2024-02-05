12f07e7c33a3cd90d01eb62605b124184080e6b1
Emerson Melo <emerson_sardinha@hotmail.com>
2024-02-05 19:08:53 -0300

    Criando tela de cadastro conclusao

--- lib/pages/cadastro_conclusao_page.dart
+++ lib/pages/cadastro_conclusao_page.dart
@@ -1,4 +1,3 @@
-import 'package:cine_app/pages/cadastro_conclusao_page.dart';
 import 'package:cine_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -7,22 +6,22 @@ import '../helper/propriedades_globais.dart';
 import '../services/navigation_service.dart';
 import '../widgets/widgets_globais.dart';
 
-class CadastroSenhaPage extends StatefulWidget {
+class CadastroConclusaoPage extends StatefulWidget {
 
-  const CadastroSenhaPage({Key? key}) : super(key: key);
+  const CadastroConclusaoPage({Key? key}) : super(key: key);
 
   @override
-  State<CadastroSenhaPage> createState() => _CadastroSenhaPageState();
+  State<CadastroConclusaoPage> createState() => _CadastroConclusaoPageState();
 }
 
-class _CadastroSenhaPageState extends State<CadastroSenhaPage> {
+class _CadastroConclusaoPageState extends State<CadastroConclusaoPage> {
 
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: criarAppBar('Cadastrar senha'),
+      appBar: criarAppBar('Conclusão'),
       backgroundColor: navbarColor,
       body: ScrollViewCustomizado(context),
     );
@@ -52,48 +51,26 @@ class _CadastroSenhaPageState extends State<CadastroSenhaPage> {
           children: [
             const SizedBox(height: 40),
             const Text(
-              'Insira uma senha.',
+              'Parabéns.',
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
             ),
             const SizedBox(height: 20),
             const Padding(
               padding: EdgeInsets.only(left: 5.0, right: 5.0),
               child: Text(
-                'Para finalizar seu cadastro, insira uma senha. Lembre-se, uma senha forte é composta por letras maiúsculas, minúsculas e números!',
+                'Seu cadastro foi concluído com sucesso. Vá para o login e entre com o e-mail e senha cadastrada.',
                 style: TextStyle(color: Colors.white60),
                 textAlign: TextAlign.center,
               ),
             ),
             const SizedBox(height: 10),
-            Lottie.asset('lib/assets/security.json', height: 240),
+            Lottie.asset('lib/assets/success.json', height: 380),
             const SizedBox(height: 10),
-            const TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                contentPadding: EdgeInsets.all(12),
-                isDense: true,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.deepPurple),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-                hintText: 'Digite uma senha forte',
-                helperText: null,
-                helperStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-              ),
-            ),
+
             Expanded(
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: botaoCustom(context, 'FINALIZAR', () => navigateToPage(context, const CadastroConclusaoPage()),
+                child: botaoCustom(context, 'LOGIN', () => navigateToPage(context, const LoginPage()),
                     Colors.purple.shade900, Colors.white, true),
               ),
             ),
